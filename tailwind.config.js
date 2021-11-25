ee28f064738a398fe5ebb3c863b77c34f7ef2afa
CptBaroud <gurvan.seveno@gmail.com>
2021-11-25 16:03:37 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    enable: true,
+    content: ['./public/*.html'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     container: {
       padding: '2rem'
+    },
+    colors : {
+      cyan: colors.cyan,
+      sky: colors.sky,
+      green: colors.green,
+      yellow: colors.amber,
+      red: colors.red,
+      orange: colors.orange,
+      gray: colors.gray,
+      blue: colors.blue,
+      white: colors.white,
+      pink: colors.pink
     },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
